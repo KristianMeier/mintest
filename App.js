79751edb0c7256b93ae3f34a3319883ec261eb5e
KristianMeier <85857919+KristianMeier@users.noreply.github.com>
2022-07-21 17:01:18 +0200

    2nd commit

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 // husk at installeret react-native-webview
 // https://www.c-sharpcorner.com/article/convert-your-website-into-a-mobile-app-with-react-native/
 // I expo, brug den midster valgmulighed "LAN".
-// Dernæst, åben en iOS eller Android simulator
+// Dernæst, åben en iOS eller Android simulator.
 
 import * as React from "react";
 import { WebView } from "react-native-webview";
